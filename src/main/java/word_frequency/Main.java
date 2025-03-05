288c94f9793ec6ae5abf63ff8d355c7800d6d684
ahmed.tantawy <ahmed.tantawy@sewedy.com.eg>
2025-03-05 13:58:16 +0200

    changed calculate time

--- src/main/java/word_frequency/Main.java
+++ src/main/java/word_frequency/Main.java
@@ -6,30 +6,32 @@ public class Main {
         long start = 0L;
         long end = 0L;
 
-        start = System.currentTimeMillis();
+
         TrieImplementation trie = new TrieImplementation();
         FileHelper.readFromFile(trie);
 
         System.out.println("=========== Trie ===========");
+        start = System.currentTimeMillis();
         System.out.println("Programming is found " + trie.findFrequency("Programming") + " times");
         end = System.currentTimeMillis();
         System.out.println("Time taken: " + (end - start) + " MS");
 
-        start = System.currentTimeMillis();
+
         ListImplementation list = new ListImplementation();
         FileHelper.readFromFile(list);
 
         System.out.println("=========== List ===========");
+        start = System.currentTimeMillis();
         System.out.println("Programming is found " + list.findFrequency("Programming") + " times");
         end = System.currentTimeMillis();
         System.out.println("Time taken: " + (end - start) + " MS");
 
 
-        start = System.currentTimeMillis();
         HashMapImplementation hashMap = new HashMapImplementation();
         FileHelper.readFromFile(hashMap);
 
         System.out.println("=========== Hash map ===========");
+        start = System.currentTimeMillis();
         System.out.println("Programming is found " + hashMap.findFrequency("Programming") + " times");
         end = System.currentTimeMillis();
         System.out.println("Time taken: " + (end - start) + " MS");
